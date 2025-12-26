b091453f3d6f45f2ce6ff44403ad2048cff34761
reypjbaliguat <reypjbaliguat@gmail.com>
2025-12-27 00:12:44 +0800

    feat: add CORS support and update dependencies for improved security

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
+import cors from "cors";
 import express from "express";
 import authRoutes from "./routes/authRoutes";
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use("/v1/api/auth", authRoutes);
