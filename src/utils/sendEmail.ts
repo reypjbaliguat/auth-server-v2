ec23e2f8d6606110b6cd392cc91298d5c1bcbe7f
reypjbaliguat <reypjbaliguat@gmail.com>
2026-02-23 03:23:07 +0800

    feat: integrate Resend for email sending functionality

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -1,15 +1,23 @@
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async (to: string, subject: string, text: string) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS },
-  });
+  //  const transporter = nodemailer.createTransport({
+  //    service: "gmail",
+  //    auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS },
+  //  });
 
-  await transporter.sendMail({
-    from: `"Your App" <${process.env.EMAIL_USER}>`,
+  //  await transporter.sendMail({
+  //    from: `"Your App" <${process.env.EMAIL_USER}>`,
+  //    to,
+  //    subject,
+  //    text,
+  //  });
+
+  await resend.emails.send({
+    from: "reypjbaliguat@gmail.com",
     to,
     subject,
-    text,
+    html: text,
   });
 };
