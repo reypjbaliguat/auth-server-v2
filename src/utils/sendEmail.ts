a576dbdcbd32ca7f66a6fbbba3ff54d766e23b44
reypjbaliguat <reypjbaliguat@gmail.com>
2026-02-23 03:46:42 +0800

    refactor: streamline email sending logic using Resend API

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -1,23 +1,17 @@
 import { Resend } from "resend";
-const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async (to: string, subject: string, text: string) => {
-  //  const transporter = nodemailer.createTransport({
-  //    service: "gmail",
-  //    auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS },
-  //  });
-
-  //  await transporter.sendMail({
-  //    from: `"Your App" <${process.env.EMAIL_USER}>`,
-  //    to,
-  //    subject,
-  //    text,
-  //  });
-
-  await resend.emails.send({
-    from: "reypjbaliguat@gmail.com",
-    to,
-    subject,
-    html: text,
-  });
+  const resend = new Resend(process.env.RESEND_API_KEY);
+  try {
+    const data = await resend.emails.send({
+      from: "onboarding@resend.dev", // default test sender
+      to,
+      subject,
+      html: `<p>${text}</p>`,
+    });
+    return data;
+  } catch (error) {
+    console.error("Email error:", error);
+    throw error;
+  }
 };
